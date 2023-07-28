ec65d0a7e7c9d7aa0d4910e58e82d782a955e0a5
ruslan <ruslanantipov@gmail.com>
2023-07-28 17:21:01 +0300

    fix

--- tests/test_ui_book_store_login.py
+++ tests/test_ui_book_store_login.py
@@ -3,11 +3,30 @@ from package.login_page import LoginPage
 from utils import attach
 import allure
 from selene import browser
-
+from selenium import webdriver
+from selene.support.shared import browser
+from selenium.webdriver.chrome.options import Options
 
 @allure.tag('ui')
 @allure.title('Авторизация пользователя')
 def test_login_form():
+    options = Options()
+    selenoid_capabilities = {
+        "browserName": "chrome",
+        "browserVersion": "100.0",
+        "selenoid:options": {
+            "enableVNC": True,
+            "enableVideo": False
+        }
+    }
+    options.capabilities.update(selenoid_capabilities)
+
+    driver = webdriver.Remote(
+        command_executor="http://user1:1234@selenoid.autotests.cloud/wd/hub",
+        options=options)
+
+    browser.config.driver = driver
+
     user = BookUser(
         username='Ivanoff',
         password='Test123#',
